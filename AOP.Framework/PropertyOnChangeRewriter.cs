1ecbffb7c8255a8eb24e52226f94d17f46698157
Jason Anderson <jason@jasonjanderson.com>
2013-09-13 13:48:51 -0500

    Init VS solution

---
+++ AOP.Framework/PropertyOnChangeRewriter.cs
@@ -0,0 +1,31 @@
+﻿using Roslyn.Compilers.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOP.Framework
+{
+    internal sealed class PropertyOnChangeRewriter : SyntaxRewriter
+    {
+        private readonly SemanticModel _semanticModel;
+
+        internal PropertyOnChangeRewriter(SemanticModel model)
+        {
+            this._semanticModel = model;
+        }
+
+        public override SyntaxNode VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+        {
+            if (node.AccessorList.Accessors.Any(SyntaxKind.SetAccessorDeclaration))
+            {
+
+            }
+
+        }
+
+
+
+    }
+}
