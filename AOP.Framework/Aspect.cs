4216ebcd4609094c4165b7678547b8fb023390eb
Dom <odiernod@gmail.com>
2013-09-14 11:24:33 -0500

    incremental base api for aop framework

---
+++ AOP.Framework/Aspect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOP.Framework
+{
+    public class Aspect
+    {
+        public List<IAdvice> Advices { get; set; }
+
+        public List<PointCut> PointCuts { get; set; }
+    }
+}
